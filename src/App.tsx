9b382b7c457f70a53aafba751830b3873ed2e9e1
Jorge Madson <jorgemadsonsv@gmail.com>
2022-05-23 21:10:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Samba Protocol</h1>
+        <p>Beats like the heart of the whole country</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
